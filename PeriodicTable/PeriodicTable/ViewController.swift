2ff3e5813fcfa9980a900419d6d4ca5f1cf6498e
Drbingbing <tw718140@gmail.com>
2023-10-13 10:32:41 +0800

    initialize project

---
+++ PeriodicTable/PeriodicTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PeriodicTable
+//
+//  Created by 鍾秉辰 on 2023/10/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
